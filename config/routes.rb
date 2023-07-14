034a53cd49f7df6f6b422ce30e063a5c5e44700d
Tech Taro <masahito.shimada@enishi-advisory.com>
2023-07-14 19:06:23 +0000

    edit_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -14,10 +15,23 @@ Rails.application.routes.draw do
     get "verify", :to => "users/registrations#verify"
     delete "logout", :to => "users/sessions#destroy"
   end
-    resources :products do
-      member do
-        get :favorite
-      end
-      resources :reviews, only: [:create]
+  
+  resource :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      get "mypage/address/edit", :to => "users#edit_address"
+      put "mypage", :to => "users#update"
+      get "mypage/edit_password", :to =>"users#edit_password"
+      put "mypage/password", :to => "users#update_password"
+      get  "mypage/favorite", :to => "users#favorite"
     end
+  end
+  
+  resources :products do
+    member do
+      get :favorite
+    end
+    resources :reviews, only: [:create]
+  end
 end
