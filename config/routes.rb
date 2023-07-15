c22d3dca5f57fa2b3a0d506ee069b46b979d8bb4
Tech Taro <masahito.shimada@enishi-advisory.com>
2023-07-15 13:32:56 +0000

    14 shopping_cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
@@ -18,6 +19,9 @@ Rails.application.routes.draw do
   
   resource :users, only: [:edit, :update] do
     collection do
+      get "cart", :to => "shopping_carts#index"
+      post "cart/create", :to => "shopping_carts#create"
+      delete "cart", :to => "shopping_carts#destroy"
       get "mypage", :to => "users#mypage"
       get "mypage/edit", :to => "users#edit"
       get "mypage/address/edit", :to => "users#edit_address"
