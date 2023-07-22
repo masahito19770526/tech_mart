e1bb607414780df7848e0ce76c9ec5a6a9f5ac4e
Tech Taro <masahito.shimada@enishi-advisory.com>
2023-07-22 11:13:32 +0000

    upload

--- config/routes.rb
+++ config/routes.rb
@@ -43,17 +43,21 @@ Rails.application.routes.draw do
   
   resource :users, only: [:edit, :update] do
     collection do
-      get "cart", :to => "shopping_carts#index"
-      post "cart/create", :to => "shopping_carts#create"
-      delete "cart", :to => "shopping_carts#destroy"
-      get "mypage", :to => "users#mypage"
-      get "mypage/edit", :to => "users#edit"
-      get "mypage/address/edit", :to => "users#edit_address"
-      put "mypage", :to => "users#update"
-      get "mypage/edit_password", :to =>"users#edit_password"
-      put "mypage/password", :to => "users#update_password"
-      get "mypage/favorite", :to => "users#favorite"
-      delete "mypage/delete", :to => "users#destroy"
+      get    "cart",               :to => "shopping_carts#index"
+      delete "cart",               :to => "shopping_carts#destroy"
+      post   "cart/create",        :to => "shopping_carts#create"
+      get    "mypage",                 :to => "users#mypage"
+      put    "mypage",                 :to => "users#update"
+      get    "mypage/edit",            :to => "users#edit"
+      get    "mypage/edit_password",   :to => "users#edit_password"
+      delete "mypage/delete",          :to => "users#destroy"
+      get    "mypage/address/edit",    :to => "users#edit_address"
+      put    "mypage/password",        :to => "users#update_password"
+      get    "mypage/favorite",        :to => "users#favorite"
+      get    "mypage/cart_history",    :to => "users#cart_history_index", :as => "mypage_cart_histories"      
+      get    "mypage/cart_history/:num", :to => "users#cart_history_show", :as => "mypage_cart_history"
+      get    "mypage/register_card",     :to => "users#register_card"
+      post   "mypage/token",             :to => "users#token"    
     end
   end
   
